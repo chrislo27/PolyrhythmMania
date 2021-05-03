86d3433c4474c6faf519cf07410bb77275150e90
chrislo27 <kinchrislo@gmail.com>
2021-05-02 17:09:39 -0700

    Add an int cache for Insets with int values 0..10

--- core/src/main/kotlin/io/github/chrislo27/paintbox/ui/area/Insets.kt
+++ core/src/main/kotlin/io/github/chrislo27/paintbox/ui/area/Insets.kt
@@ -1,10 +1,25 @@
 package io.github.chrislo27.paintbox.ui.area
 
+import kotlin.math.floor
+
 
 data class Insets(val top: Float, val bottom: Float, val left: Float, val right: Float) {
     companion object {
         val ZERO: Insets = Insets(0f, 0f, 0f, 0f)
+
+        private val intCache: Array<Insets> by lazy {
+            Array(11) { i ->
+                val f = i.toFloat()
+                Insets(f, f, f, f)
+            }
+        }
+
+        operator fun invoke(all: Float): Insets {
+            val toInt = all.toInt()
+            if (floor(all) == all && toInt in intCache.indices) {
+                return intCache[toInt]
+            }
+            return Insets(all, all, all, all)
+        }
     }
-    
-    constructor(all: Float) : this(all, all, all, all)
 }
