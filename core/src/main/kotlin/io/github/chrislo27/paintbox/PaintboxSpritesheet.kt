e327ebae02c0e2d14ee64b6498db947357419c2b
chrislo27 <kinchrislo@gmail.com>
2021-05-14 16:07:52 -0700

    Add radio button sprites

--- core/src/main/kotlin/io/github/chrislo27/paintbox/PaintboxSpritesheet.kt
+++ core/src/main/kotlin/io/github/chrislo27/paintbox/PaintboxSpritesheet.kt
@@ -12,4 +12,6 @@ class PaintboxSpritesheet(val texture: Texture) {
     val checkboxEmpty: TextureRegion = TextureRegion(texture, 1, 35, 64, 64)
     val checkboxCheck: TextureRegion = TextureRegion(texture, 68, 35, 64, 64)
     val checkboxX: TextureRegion = TextureRegion(texture, 135, 35, 64, 64)
+    val radioButtonEmpty: TextureRegion = TextureRegion(texture, 202, 35, 64, 64)
+    val radioButtonFilled: TextureRegion = TextureRegion(texture, 269, 35, 64, 64)
 }
