740ac2119e86716ac954af1c81ff297e58041cac
chrislo27 <kinchrislo@gmail.com>
2021-06-09 11:51:58 -0700

    Fix wrong decimal symbols in ms time conversion

--- core/src/main/kotlin/polyrhythmmania/util/DecimalFormats.kt
+++ core/src/main/kotlin/polyrhythmmania/util/DecimalFormats.kt
@@ -7,8 +7,10 @@ import java.text.DecimalFormatSymbols
 object DecimalFormats {
     private val formats: MutableMap<String, DecimalFormat> = mutableMapOf()
     
+    fun createDecimalFormatSymbols(): DecimalFormatSymbols = DecimalFormatSymbols()
+    
     operator fun get(format: String): DecimalFormat =
-            formats.getOrPut(format) { DecimalFormat(format, DecimalFormatSymbols()) }
+            formats.getOrPut(format) { DecimalFormat(format, createDecimalFormatSymbols()) }
     
     fun format(format: String, value: Float): String = get(format).format(value.toDouble())
     fun format(format: String, value: Double): String = get(format).format(value)
--- core/src/main/kotlin/polyrhythmmania/util/TimeUtils.kt
+++ core/src/main/kotlin/polyrhythmmania/util/TimeUtils.kt
@@ -1,9 +1,12 @@
 package polyrhythmmania.util
 
+import java.text.DecimalFormatSymbols
 import kotlin.math.abs
 
 
 object TimeUtils {
+    
+    private val decimalFormatSymbols: DecimalFormatSymbols = DecimalFormats.createDecimalFormatSymbols()
     
     fun convertMsToTimestamp(ms: Float): String {
         val msAbs = abs(ms)
@@ -11,6 +14,6 @@ object TimeUtils {
         val sec = (msAbs / 1000 % 60).toInt()
         val msPart = (msAbs % 1000).toInt()
 
-        return "${if (ms < 0) "-" else ""}${DecimalFormats.format("00", min)}:${DecimalFormats.format("00", sec)}.${DecimalFormats.format("000", msPart)}"
+        return "${if (ms < 0) "${decimalFormatSymbols.minusSign}" else ""}${DecimalFormats.format("00", min)}:${DecimalFormats.format("00", sec)}${decimalFormatSymbols.decimalSeparator}${DecimalFormats.format("000", msPart)}"
     }
 }
