195ae0c9d2557c277cca2a25b6cc1147b6b5d185
chrislo27 <kinchrislo@gmail.com>
2021-06-30 20:40:33 -0700

    Don't manipulate xOffset in TextLabel by scaleX
    Feels like I've been changing either this or TextBlock too many times...

--- core/src/main/kotlin/paintbox/ui/control/TextLabel.kt
+++ core/src/main/kotlin/paintbox/ui/control/TextLabel.kt
@@ -159,8 +159,8 @@ open class TextLabelSkin(element: TextLabel) : Skin<TextLabel>(element) {
         val textHeight = text.height * scaleY
         val xOffset: Float = when {
             Align.isLeft(align) -> 0f + bgPaddingInsets.left
-            Align.isRight(align) -> (w - ((if (compressX) (min(textWidth, w)) else textWidth) / scaleX + bgPaddingInsets.right))
-            else -> (w - (if (compressX) min(textWidth, w) else textWidth)) / 2f / scaleX
+            Align.isRight(align) -> (w - ((if (compressX) (min(textWidth, w)) else textWidth) + bgPaddingInsets.right))
+            else -> (w - (if (compressX) min(textWidth, w) else textWidth)) / 2f
         }
         val firstCapHeight = text.firstCapHeight * scaleY
         val yOffset: Float = when {
