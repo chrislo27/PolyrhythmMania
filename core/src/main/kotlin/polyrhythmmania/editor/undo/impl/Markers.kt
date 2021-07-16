fb4377431d5cf8136e7a3e6d2c9b6921b16b20c6
chrislo27 <kinchrislo@gmail.com>
2021-07-15 22:38:23 -0700

    Fix undoing music sync marker not updating waveform

--- core/src/main/kotlin/polyrhythmmania/editor/undo/impl/Markers.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/undo/impl/Markers.kt
@@ -7,12 +7,18 @@ import polyrhythmmania.editor.undo.ReversibleAction
 
 class MoveMarkerAction(val marker: MarkerType, val previous: Float, var next: Float)
     : ReversibleAction<Editor> {
-
+    
     override fun redo(context: Editor) {
         context.markerMap.getValue(marker).beat.set(next)
+        if (marker == MarkerType.MUSIC_FIRST_BEAT) {
+            context.compileEditorMusicInfo()
+        }
     }
 
     override fun undo(context: Editor) {
         context.markerMap.getValue(marker).beat.set(previous)
+        if (marker == MarkerType.MUSIC_FIRST_BEAT) {
+            context.compileEditorMusicInfo()
+        }
     }
 }
