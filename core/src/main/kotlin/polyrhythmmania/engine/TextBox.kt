225562eaab18c8570671593567cd23431aeef98c
chrislo27 <kinchrislo@gmail.com>
2021-09-01 17:30:22 -0700

    Add new style of text box

--- core/src/main/kotlin/polyrhythmmania/engine/TextBox.kt
+++ core/src/main/kotlin/polyrhythmmania/engine/TextBox.kt
@@ -1,8 +1,12 @@
 package polyrhythmmania.engine
 
 
+enum class TextBoxStyle {
+    DIALOGUE, BLACK;
+}
+
 data class TextBox(val text: String, val requiresInput: Boolean,
-                   val secsBeforeCanInput: Float = 0.5f) {
+                   val secsBeforeCanInput: Float = 0.5f, val style: TextBoxStyle = TextBoxStyle.BLACK) {
     fun toActive(): ActiveTextBox = ActiveTextBox(this)
 }
 
@@ -10,5 +14,4 @@ data class ActiveTextBox(val textBox: TextBox) {
     var secondsTimer: Float = textBox.secsBeforeCanInput
     var isADown: Boolean = false
     var wasSoundInterfacePaused: Boolean = false
-
 }
