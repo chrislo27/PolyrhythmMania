54d18f95574cb6bf90bc8852d0b4eec7ad549775
chrislo27 <kinchrislo@gmail.com>
2021-09-04 22:15:46 -0700

    Fix playing an endless mode/side mode not showing correct elapsed time
    Forward-port of 5e5eab0f

--- core/src/main/kotlin/polyrhythmmania/discord/DefaultPresences.kt
+++ core/src/main/kotlin/polyrhythmmania/discord/DefaultPresences.kt
@@ -11,9 +11,9 @@ object DefaultPresences {
     fun inEditor(): Presence = Presence(state = "Editing a level")
     fun playingLevel(): Presence = Presence(state = "Playing a level")
     fun playingPractice(): Presence = Presence(state = "Playing a practice mode")
-    fun playingEndlessMode(): Presence = Presence(state = "Playing Endless Mode")
-    fun playingDailyChallenge(date: LocalDate): Presence = Presence(state = "Playing the Daily Challenge (${date.format(DateTimeFormatter.ISO_DATE)})")
-    fun playingDunk(): Presence = Presence(state = "Playing Polyrhythm: Dunk")
+    fun playingEndlessMode(): Presence = Presence(state = "Playing Endless Mode", startTimestamp = System.currentTimeMillis() / 1000L)
+    fun playingDailyChallenge(date: LocalDate): Presence = Presence(state = "Playing the Daily Challenge (${date.format(DateTimeFormatter.ISO_DATE)})", startTimestamp = System.currentTimeMillis() / 1000L)
+    fun playingDunk(): Presence = Presence(state = "Playing Polyrhythm: Dunk", startTimestamp = System.currentTimeMillis() / 1000L)
     fun playingAssemble(): Presence = Presence(state = "Playing Polyrhythm: Assemble")
 
 }
