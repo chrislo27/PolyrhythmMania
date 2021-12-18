ec28b2501d72d23e674b9ffbb0fa62b900e8be8a
chrislo27 <kinchrislo@gmail.com>
2021-12-17 21:08:44 -0800

    DEV version

--- core/src/main/kotlin/polyrhythmmania/PRMania.kt
+++ core/src/main/kotlin/polyrhythmmania/PRMania.kt
@@ -14,7 +14,7 @@ object PRMania {
     const val GITHUB = "https://github.com/chrislo27/PolyrhythmMania"
     const val HOMEPAGE = "https://polyrhythmmania.rhre.dev"
     const val DONATE_LINK = "https://www.paypal.com/donate/?hosted_button_id=9JLGHKZNWLLQ8"
-    val VERSION: Version = Version(1, 1, 1, "20211207a")
+    val VERSION: Version = Version(1, 1, 2, "dev_20211218a")
     const val WIDTH: Int = 1280 //1080
     const val HEIGHT: Int = 720
     val DEFAULT_SIZE: WindowSize = WindowSize(WIDTH, HEIGHT)
