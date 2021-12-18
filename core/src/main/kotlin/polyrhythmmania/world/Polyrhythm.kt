91ceba5ac1a5e52876085ca27c25251ec3dc518c
chrislo27 <kinchrislo@gmail.com>
2021-12-17 22:24:48 -0800

    Increase leniency of rod collision with piston edge while mid-animation

--- core/src/main/kotlin/polyrhythmmania/world/Polyrhythm.kt
+++ core/src/main/kotlin/polyrhythmmania/world/Polyrhythm.kt
@@ -398,18 +398,21 @@ class EntityRodPR(world: World, deployBeat: Float, val row: Row,
                 }
                 
                 if (next.active && posYCheck in next.position.y..(next.position.y + heightOfNext - (1f / 32f))) {
-//                    println("Collision: my Y ${prevPosY} ${posYCheck}   ${next.position.y}  ${next.position.y + heightOfNext}")
-                    
-                    collision.collidedWithWall = true
-//                    println("$seconds Collided with wall: currentIndex = ${currentIndexFloat}  x = ${this.position.x}")
-                    this.position.x = currentIndex + 0.7f + row.startX
-//                    println("$seconds After setting X: currentIndex would be ${this.position.x - row.startX}   x = ${this.position.x}")
+                    // Exclude the lip of pistons if they are NOT fully extended
+                    if (!(next.type != EntityPiston.Type.PLATFORM && next.pistonState == EntityPiston.PistonState.PARTIALLY_EXTENDED && (posYCheck >= next.position.y + 1f && posYCheck <= next.position.y + heightOfNext))) {
+//                        println("Collision: my Y ${prevPosY} ${posYCheck}   ${next.position.y}  ${next.position.y + heightOfNext}  ${next.type}  ${next.pistonState}")
 
-                    playSfxSideCollision(engine)
+                        collision.collidedWithWall = true
+//                        println("$seconds Collided with wall: currentIndex = ${currentIndexFloat}  x = ${this.position.x}  y = ${this.position.y}")
+                        this.position.x = currentIndex + 0.7f + row.startX
+//                        println("$seconds After setting X: currentIndex would be ${this.position.x - row.startX}   x = ${this.position.x}")
 
-                    // Standard collision detection will not take effect before index = -1
-                    if (explodeAtSec == Float.MAX_VALUE) {
-                        explodeAtSec = seconds + EXPLODE_DELAY_SEC
+                        playSfxSideCollision(engine)
+
+                        // Standard collision detection will not take effect before index = -1
+                        if (explodeAtSec == Float.MAX_VALUE) {
+                            explodeAtSec = seconds + EXPLODE_DELAY_SEC
+                        }
                     }
                 }
             }
