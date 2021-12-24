7d46ab3cc17541f0e708436337b6f072133065fc
chrislo27 <kinchrislo@gmail.com>
2021-12-24 11:23:01 -0800

    Add view by category in achievements menu

--- core/src/main/kotlin/polyrhythmmania/achievements/Achievements.kt
+++ core/src/main/kotlin/polyrhythmmania/achievements/Achievements.kt
@@ -47,32 +47,32 @@ object Achievements {
     /**
      * Triggered when getting a score of at least 25 in Endless Mode, any settings.
      */
-    val endlessScore25 = register(ScoreThreshold("endless_score_25", STATISTICAL, ENDLESS_MODE, false, 25))
+    val endlessScore25 = register(ScoreThreshold("endless_score_25", OBJECTIVE, ENDLESS_MODE, false, 25))
     /**
      * Triggered when getting a score of at least 50 in Endless Mode, any settings.
      */
-    val endlessScore50 = register(ScoreThreshold("endless_score_50", STATISTICAL, ENDLESS_MODE, false, 50))
+    val endlessScore50 = register(ScoreThreshold("endless_score_50", OBJECTIVE, ENDLESS_MODE, false, 50))
     /**
      * Triggered when getting a score of at least 75 in Endless Mode, any settings.
      */
-    val endlessScore75 = register(ScoreThreshold("endless_score_75", STATISTICAL, ENDLESS_MODE, false, 75))
+    val endlessScore75 = register(ScoreThreshold("endless_score_75", OBJECTIVE, ENDLESS_MODE, false, 75))
     /**
      * Triggered when getting a score of at least 100 in Endless Mode, any settings.
      */
-    val endlessScore100 = register(ScoreThreshold("endless_score_100", STATISTICAL, ENDLESS_MODE, false, 100))
+    val endlessScore100 = register(ScoreThreshold("endless_score_100", OBJECTIVE, ENDLESS_MODE, false, 100))
     /**
      * Triggered when getting a score of at least 125 in Endless Mode, any settings.
      */
-    val endlessScore125 = register(ScoreThreshold("endless_score_125", STATISTICAL, ENDLESS_MODE, false, 125))
+    val endlessScore125 = register(ScoreThreshold("endless_score_125", OBJECTIVE, ENDLESS_MODE, false, 125))
     
     /**
      * Triggered when getting a score of at least 100 in Endless Mode with life regen disabled.
      */
-    val endlessNoLifeRegen100 = register(ScoreThreshold("endless_no_regen_100", STATISTICAL, ENDLESS_MODE, false, 100))
+    val endlessNoLifeRegen100 = register(ScoreThreshold("endless_no_regen_100", CHALLENGE, ENDLESS_MODE, false, 100))
     /**
      * Triggered when getting a score of at least 100 in Endless Mode in daredevil mode.
      */
-    val endlessDaredevil100 = register(ScoreThreshold("endless_daredevil_100", STATISTICAL, ENDLESS_MODE, false, 100))
+    val endlessDaredevil100 = register(ScoreThreshold("endless_daredevil_100", CHALLENGE, ENDLESS_MODE, false, 100))
 
 
     /**
@@ -90,23 +90,23 @@ object Achievements {
     /**
      * Triggered when getting a score of at least 25 in Daily Challenge.
      */
-    val dailyScore25 = register(ScoreThreshold("daily_score_25", STATISTICAL, DAILY, false, 25))
+    val dailyScore25 = register(ScoreThreshold("daily_score_25", OBJECTIVE, DAILY, false, 25))
     /**
      * Triggered when getting a score of at least 50 in Daily Challenge.
      */
-    val dailyScore50 = register(ScoreThreshold("daily_score_50", STATISTICAL, DAILY, false, 50))
+    val dailyScore50 = register(ScoreThreshold("daily_score_50", OBJECTIVE, DAILY, false, 50))
     /**
      * Triggered when getting a score of at least 75 in Daily Challenge.
      */
-    val dailyScore75 = register(ScoreThreshold("daily_score_75", STATISTICAL, DAILY, false, 75))
+    val dailyScore75 = register(ScoreThreshold("daily_score_75", OBJECTIVE, DAILY, false, 75))
     /**
      * Triggered when getting a score of at least 100 in Daily Challenge.
      */
-    val dailyScore100 = register(ScoreThreshold("daily_score_100", STATISTICAL, DAILY, false, 100))
+    val dailyScore100 = register(ScoreThreshold("daily_score_100", OBJECTIVE, DAILY, false, 100))
     /**
      * Triggered when getting a score of at least 125 in Daily Challenge.
      */
-    val dailyScore125 = register(ScoreThreshold("daily_score_125", STATISTICAL, DAILY, false, 125))
+    val dailyScore125 = register(ScoreThreshold("daily_score_125", OBJECTIVE, DAILY, false, 125))
     
     
     /**
@@ -120,19 +120,19 @@ object Achievements {
     /**
      * Triggered when getting a score of at least 10 in Dunk.
      */
-    val dunkScore10 = register(ScoreThreshold("dunk_score_10", STATISTICAL, EXTRAS, false, 10))
+    val dunkScore10 = register(ScoreThreshold("dunk_score_10", OBJECTIVE, EXTRAS, false, 10))
     /**
      * Triggered when getting a score of at least 20 in Dunk.
      */
-    val dunkScore20 = register(ScoreThreshold("dunk_score_20", STATISTICAL, EXTRAS, false, 20))
+    val dunkScore20 = register(ScoreThreshold("dunk_score_20", OBJECTIVE, EXTRAS, false, 20))
     /**
      * Triggered when getting a score of at least 30 in Dunk.
      */
-    val dunkScore30 = register(ScoreThreshold("dunk_score_30", STATISTICAL, EXTRAS, false, 30))
+    val dunkScore30 = register(ScoreThreshold("dunk_score_30", OBJECTIVE, EXTRAS, false, 30))
     /**
      * Triggered when getting a score of at least 50 in Dunk.
      */
-    val dunkScore50 = register(ScoreThreshold("dunk_score_50", STATISTICAL, EXTRAS, false, 50))
+    val dunkScore50 = register(ScoreThreshold("dunk_score_50", OBJECTIVE, EXTRAS, false, 50))
 
 
     /**
@@ -162,37 +162,37 @@ object Achievements {
     /**
      * Triggered when at least 15 unique levels are in the Library.
      */
-    val libraryCollection15 = register(NumericalThreshold("library_collection_15", OBJECTIVE, GENERAL, false, 15))
+    val libraryCollection15 = register(NumericalThreshold("library_collection_15", STATISTICAL, GENERAL, false, 15))
     
     
     /**
      * Triggered when X successful inputs are gotten.
      */
-    val successfulInputs1000 = register(StatTriggered("successful_inputs_1000", OBJECTIVE, GENERAL, false, GlobalStats.inputsGottenTotal, 1000))
+    val successfulInputs1000 = register(StatTriggered("successful_inputs_1000", STATISTICAL, GENERAL, false, GlobalStats.inputsGottenTotal, 1000))
     /**
      * Triggered when X ace inputs are gotten.
      */
-    val aceInputs1000 = register(StatTriggered("ace_inputs_1000", OBJECTIVE, GENERAL, false, GlobalStats.inputsGottenAce, 1000))
+    val aceInputs1000 = register(StatTriggered("ace_inputs_1000", STATISTICAL, GENERAL, false, GlobalStats.inputsGottenAce, 1000))
     /**
      * Triggered when X rods have exploded.
      */
-    val rodsExploded10000 = register(StatTriggered("rods_exploded_10000", OBJECTIVE, GENERAL, false, GlobalStats.rodsExploded, 10000))
+    val rodsExploded10000 = register(StatTriggered("rods_exploded_10000", STATISTICAL, GENERAL, false, GlobalStats.rodsExploded, 10000))
     /**
      * Triggered when a Skill Star is earned for the 5th time.
      */
-    val skillStar5 = register(StatTriggered("skill_star_5", OBJECTIVE, GENERAL, false, GlobalStats.skillStarsEarned, 5))
+    val skillStar5 = register(StatTriggered("skill_star_5", STATISTICAL, GENERAL, false, GlobalStats.skillStarsEarned, 5))
     /**
      * Triggered when a Skill Star is earned for the 100th time.
      */
-    val skillStar100 = register(StatTriggered("skill_star_100", OBJECTIVE, GENERAL, false, GlobalStats.skillStarsEarned, 100))
+    val skillStar100 = register(StatTriggered("skill_star_100", STATISTICAL, GENERAL, false, GlobalStats.skillStarsEarned, 100))
     /**
      * Triggered when 30 unique levels have been played.
      */
-    val uniqueLevelsPlayed30 = register(StatTriggered("unique_levels_played_30", OBJECTIVE, GENERAL, false, GlobalStats.timesPlayedUniqueCustomLevel, 30))
+    val uniqueLevelsPlayed30 = register(StatTriggered("unique_levels_played_30", STATISTICAL, GENERAL, false, GlobalStats.timesPlayedUniqueCustomLevel, 30))
     /**
      * Triggered when 10 hours of total playtime have been reached.
      */
-    val playtimeHours10 = register(StatTriggered("playtime_hours_10", OBJECTIVE, GENERAL, false, GlobalStats.totalPlayTime, 10 * 60 * 60))
+    val playtimeHours10 = register(StatTriggered("playtime_hours_10", STATISTICAL, GENERAL, false, GlobalStats.totalPlayTime, 10 * 60 * 60))
 
     
     /**
@@ -203,11 +203,11 @@ object Achievements {
      * Triggered in Endless Mode when paused in between a pattern and the point is still awarded at score 50 or greater.
      * NB: This will also trigger if the new score is 50.
      */
-    val endlessPauseBetweenInputs = register(ScoreThreshold("endless_pause_between_inputs", OBJECTIVE, ENDLESS_MODE, false, 50))
+    val endlessPauseBetweenInputs = register(ScoreThreshold("endless_pause_between_inputs", CHALLENGE, ENDLESS_MODE, false, 50))
     /**
      * Triggered in Endless Mode when getting a score of at least 50 while the master volume is 0.
      */
-    val endlessSilent50 = register(ScoreThreshold("endless_silent_50", OBJECTIVE, ENDLESS_MODE, false, 50))
+    val endlessSilent50 = register(ScoreThreshold("endless_silent_50", CHALLENGE, ENDLESS_MODE, false, 50))
     /**
      * Triggered after every Tutorial (I + II) has been completed.
      */
@@ -243,6 +243,10 @@ object Achievements {
         }
     }
     
+//    init {
+//        dumpAchievementList()
+//    }
+    
     private fun <A : Achievement> register(ach: A): A {
         _achIDMap[ach.id] = ach
         if (ach is StatTriggered) {
@@ -257,6 +261,16 @@ object Achievements {
     }
     
     // -----------------------------------------------------------------------------------------------------------------
+    
+    fun dumpAchievementList() {
+        Paintbox.LOGGER.debug("\n" + achievementIDMap.values.groupBy { it.category }.entries.sortedBy { it.key }.joinToString(separator = "\n") { (category, list) ->
+            val listStr = list.joinToString(separator = "\n-------------------------------------------\n") { ach ->
+                "${ach.getLocalizedName().getOrCompute()}\nRank: ${ach.rank} | Hidden? ${ach.isHidden} | ID: ${ach.id}\n${ach.getLocalizedDesc().getOrCompute()}"
+            }
+            
+            "\n\n\n===========================================\nCategory $category:\n$listStr"
+        } + "\n")
+    }
     
     fun clearAllFulfilledAchievements() {
         _achFulfillmentMap.set(emptyMap())
--- core/src/main/kotlin/polyrhythmmania/screen/mainmenu/menu/AchievementsMenu.kt
+++ core/src/main/kotlin/polyrhythmmania/screen/mainmenu/menu/AchievementsMenu.kt
@@ -11,11 +11,9 @@ import paintbox.binding.Var
 import paintbox.font.Markup
 import paintbox.font.TextRun
 import paintbox.registry.AssetRegistry
-import paintbox.ui.Anchor
-import paintbox.ui.ImageIcon
-import paintbox.ui.ImageRenderingMode
-import paintbox.ui.Pane
+import paintbox.ui.*
 import paintbox.ui.area.Insets
+import paintbox.ui.control.ComboBox
 import paintbox.ui.control.ScrollPane
 import paintbox.ui.control.ScrollPaneSkin
 import paintbox.ui.control.TextLabel
@@ -38,6 +36,9 @@ import java.time.format.DateTimeFormatter
 
 class AchievementsMenu(menuCol: MenuCollection) : StandardMenu(menuCol) {
 
+    private val totalProgressLabel: TextLabel
+    private val panePerCategory: Map<AchievementCategory, UIElement>
+    
     init {
         this.setSize(MMMenu.WIDTH_LARGE)
         this.titleText.bind { Localization.getVar("mainMenu.achievements.title").use() }
@@ -77,121 +78,158 @@ class AchievementsMenu(menuCol: MenuCollection) : StandardMenu(menuCol) {
             this.spacing.set(4f)
             this.margin.set(Insets(0f, 0f, 0f, 16f))
         }
-
-
-        vbox.temporarilyDisableLayouts {
-            val percentFormat = DecimalFormats["#0.#"]
-            
-            vbox += TextLabel(binding = { 
-                AchievementsL10N.getVar("achievement.progress", Var {
-                    val map = Achievements.fulfillmentMap.use()
-                    val numGotten = map.size
-                    val numTotal = Achievements.achievementIDMap.size
-                    val percentageWhole = (100f * numGotten / numTotal).coerceIn(0f, 100f)
-                    listOf(percentFormat.format(percentageWhole), numGotten, numTotal)
-                }).use()
-                                        }, font = main.fontMainMenuHeading).apply {
-                this.textColor.set(CreditsMenu.HEADING_TEXT_COLOR)
-                this.bounds.height.set(48f)
-                this.padding.set(Insets(8f))
-                this.setScaleXY(0.75f)
-                this.renderAlign.set(Align.center)
-            }
-            
-            val headingMarkup = Markup(mapOf(), TextRun(main.fontMainMenuHeading, ""), Markup.FontStyles.ALL_USING_DEFAULT_FONT)
-            val descMarkup = Markup(mapOf(
-                    Markup.FONT_NAME_ITALIC to main.fontMainMenuItalic,
-                    Markup.FONT_NAME_BOLDITALIC to main.fontMainMenuItalic,
-                    "prmania_icons" to main.fontIcons,
-                    "rodin" to main.fontMainMenuRodin,
-                    "thin" to main.fontMainMenuThin
-            ), TextRun(font, ""), Markup.FontStyles.ALL_USING_BOLD_ITALIC)
-            val statProgressColor = "9FD677"
-            val completedTextureReg = TextureRegion(AssetRegistry.get<Texture>("achievements_completed_mark"))
-            AchievementCategory.VALUES.forEach { category ->
-                val achievementsInCategory = Achievements.achievementIDMap.values.filter { it.category == category }
-                
-                vbox += TextLabel(binding = { 
-                    AchievementsL10N.getVar("achievement.categoryProgress", Var {
-                        val map = Achievements.fulfillmentMap.use()
-                        val numGotten = map.keys.count { it.category == category }
-                        val numTotal = achievementsInCategory.size
-                        val percentageWhole = (100f * numGotten / numTotal).coerceIn(0f, 100f)
-                        listOf(AchievementsL10N.getVar(category.toLocalizationID()).use(), percentFormat.format(percentageWhole), numGotten, numTotal)
-                    }).use() 
-                }).apply { 
-                    this.textColor.set(Color().grey(0.35f))
-                    this.bounds.height.set(56f)
-                    this.padding.set(Insets(16f, 8f, 0f, 32f))
-                    this.renderAlign.set(Align.bottomLeft)
-                    this.setScaleXY(0.75f)
-                    this.markup.set(headingMarkup)
-                }
-                
-                achievementsInCategory.forEach { achievement ->
-                    val achievementEarned = BooleanVar { Achievements.fulfillmentMap.use()[achievement] != null }    
-                    val entire = Pane().apply {
-                        this += ImageIcon(null, renderingMode = ImageRenderingMode.MAINTAIN_ASPECT_RATIO).apply {
-                            Anchor.TopLeft.configure(this)
-                            this.bindWidthToSelfHeight()
-                            this.textureRegion.set(TextureRegion(AssetRegistry.get<Texture>("tileset_missing_tex"))) // FIXME
-                        }
-                        this += ImageIcon(completedTextureReg, renderingMode = ImageRenderingMode.MAINTAIN_ASPECT_RATIO).apply {
-                            Anchor.TopRight.configure(this)
-                            this.bindWidthToSelfHeight()
-                            this.visible.bind { achievementEarned.use() }
-                            this.tooltipElement.set(createTooltip(AchievementsL10N.getVar("achievement.unlockedTooltip", Var {
-                                listOf(ZonedDateTime.ofInstant(Achievements.fulfillmentMap.use()[achievement]?.gotAt ?: Instant.EPOCH, ZoneId.systemDefault()).format(DateTimeFormatter.RFC_1123_DATE_TIME))
-                            })))
-                        }
-                        this += Pane().apply {
-                            Anchor.TopCentre.configure(this)
-                            this.bindWidthToParent(adjustBinding = {
-                                -(((parent.use()?.contentZone?.height?.use() ?: 0f) + 8f) * 2)
-                            })
-
-                            val statProgress: ReadOnlyVar<String> = if (achievement is Achievement.StatTriggered && achievement.showProgress) {
-                                val formatter = achievement.stat.formatter
-                                val formattedValue = formatter.format(achievement.stat.value)
-                                val formattedThreshold = formatter.format(IntVar(achievement.threshold))
-                                AchievementsL10N.getVar("achievement.statProgress", Var {
-                                    listOf(formattedValue.use(), formattedThreshold.use())
-                                })
-                            } else Var("")
-                            this += TextLabel(binding = {
-                                if (achievement.isHidden && !achievementEarned.use()) {
-                                    "[i color=#9f9f9f]${AchievementsL10N.getVar("achievement.hidden.name").use()}[]"
-                                } else {
-                                    "[color=#${achievement.rank.color.toString()} scale=1.0 lineheight=0.75]${achievement.getLocalizedName().use()} [color=#$statProgressColor scale=0.75] ${statProgress.use()}[]\n[][color=LIGHT_GRAY scale=0.75 lineheight=0.9]${if (achievement.isHidden) "${AchievementsL10N.getVar("achievement.hidden.desc").use()} " else ""}${achievement.getLocalizedDesc().use()}[]"
-                                }
-                            }).apply {
-                                Anchor.TopLeft.configure(this)
-                                this.setScaleXY(1f)
-                                this.renderAlign.set(Align.left)
-                                this.padding.set(Insets.ZERO)
-                                this.markup.set(descMarkup)
-                                this.doLineWrapping.set(true)
-                            }
-                        }
-                    }
-                    vbox += RectElement(Color.DARK_GRAY).apply { 
-                        this.bounds.height.set(72f)
-                        this.padding.set(Insets(6f))
-                        this += entire
-                    }
-                }
-            }
-            
+        
+        val percentFormat = DecimalFormats["#0.#"]
+        val headingMarkup = Markup(mapOf(), TextRun(main.fontMainMenuHeading, ""), Markup.FontStyles.ALL_USING_DEFAULT_FONT)
+        val descMarkup = Markup(mapOf(
+                Markup.FONT_NAME_ITALIC to main.fontMainMenuItalic,
+                Markup.FONT_NAME_BOLDITALIC to main.fontMainMenuItalic,
+                "prmania_icons" to main.fontIcons,
+                "rodin" to main.fontMainMenuRodin,
+                "thin" to main.fontMainMenuThin
+        ), TextRun(font, ""), Markup.FontStyles.ALL_USING_BOLD_ITALIC)
+        val statProgressColor = "9FD677"
+        val completedTextureReg = TextureRegion(AssetRegistry.get<Texture>("achievements_completed_mark"))
+        panePerCategory = linkedMapOf()
+        
+        totalProgressLabel = TextLabel(binding = {
+            AchievementsL10N.getVar("achievement.totalProgress", Var {
+                val map = Achievements.fulfillmentMap.use()
+                val numGotten = map.size
+                val numTotal = Achievements.achievementIDMap.size
+                val percentageWhole = (100f * numGotten / numTotal).coerceIn(0f, 100f)
+                listOf(percentFormat.format(percentageWhole), numGotten, numTotal)
+            }).use()
+        }, font = main.fontMainMenuHeading).apply {
+            this.textColor.set(CreditsMenu.HEADING_TEXT_COLOR)
+            this.bounds.height.set(48f)
+            this.padding.set(Insets(8f))
+            this.setScaleXY(0.75f)
+            this.renderAlign.set(Align.center)
         }
 
-        vbox.sizeHeightToChildren(100f)
-        scrollPane.setContent(vbox)
+
+        AchievementCategory.VALUES.forEach { category ->
+            val entireVBox = VBox().apply {
+                spacing.set(vbox.spacing.get())
+            }
+            val achievementsInCategory = Achievements.achievementIDMap.values.filter { it.category == category }
+
+            entireVBox += TextLabel(binding = {
+                AchievementsL10N.getVar("achievement.categoryProgress", Var {
+                    val map = Achievements.fulfillmentMap.use()
+                    val numGotten = map.keys.count { it.category == category }
+                    val numTotal = achievementsInCategory.size
+                    val percentageWhole = (100f * numGotten / numTotal).coerceIn(0f, 100f)
+                    listOf(AchievementsL10N.getVar(category.toLocalizationID()).use(), percentFormat.format(percentageWhole), numGotten, numTotal)
+                }).use()
+            }).apply {
+                this.textColor.set(Color().grey(0.35f))
+                this.bounds.height.set(56f)
+                this.padding.set(Insets(16f, 8f, 0f, 32f))
+                this.renderAlign.set(Align.bottomLeft)
+                this.setScaleXY(0.75f)
+                this.markup.set(headingMarkup)
+            }
+
+            achievementsInCategory.forEach { achievement ->
+                val achievementEarned = BooleanVar { Achievements.fulfillmentMap.use()[achievement] != null }
+                val entire = Pane().apply {
+                    this += ImageIcon(null, renderingMode = ImageRenderingMode.MAINTAIN_ASPECT_RATIO).apply {
+                        Anchor.TopLeft.configure(this)
+                        this.bindWidthToSelfHeight()
+                        this.textureRegion.set(TextureRegion(AssetRegistry.get<Texture>("tileset_missing_tex"))) // FIXME
+                    }
+                    this += ImageIcon(completedTextureReg, renderingMode = ImageRenderingMode.MAINTAIN_ASPECT_RATIO).apply {
+                        Anchor.TopRight.configure(this)
+                        this.bindWidthToSelfHeight()
+                        this.visible.bind { achievementEarned.use() }
+                        this.tooltipElement.set(createTooltip(AchievementsL10N.getVar("achievement.unlockedTooltip", Var {
+                            listOf(ZonedDateTime.ofInstant(Achievements.fulfillmentMap.use()[achievement]?.gotAt ?: Instant.EPOCH, ZoneId.systemDefault()).format(DateTimeFormatter.RFC_1123_DATE_TIME))
+                        })))
+                    }
+                    this += Pane().apply {
+                        Anchor.TopCentre.configure(this)
+                        this.bindWidthToParent(adjustBinding = {
+                            -(((parent.use()?.contentZone?.height?.use() ?: 0f) + 8f) * 2)
+                        })
+
+                        val statProgress: ReadOnlyVar<String> = if (achievement is Achievement.StatTriggered && achievement.showProgress) {
+                            val formatter = achievement.stat.formatter
+                            val formattedValue = formatter.format(achievement.stat.value)
+                            val formattedThreshold = formatter.format(IntVar(achievement.threshold))
+                            AchievementsL10N.getVar("achievement.statProgress", Var {
+                                listOf(formattedValue.use(), formattedThreshold.use())
+                            })
+                        } else Var("")
+                        this += TextLabel(binding = {
+                            if (achievement.isHidden && !achievementEarned.use()) {
+                                "[i color=#9f9f9f]${AchievementsL10N.getVar("achievement.hidden.name").use()}[]"
+                            } else {
+                                "[color=#${achievement.rank.color.toString()} scale=1.0 lineheight=0.75]${achievement.getLocalizedName().use()} [color=#$statProgressColor scale=0.75] ${statProgress.use()}[]\n[][color=LIGHT_GRAY scale=0.75 lineheight=0.9]${if (achievement.isHidden) "${AchievementsL10N.getVar("achievement.hidden.desc").use()} " else ""}${achievement.getLocalizedDesc().use()}[]"
+                            }
+                        }).apply {
+                            Anchor.TopLeft.configure(this)
+                            this.setScaleXY(1f)
+                            this.renderAlign.set(Align.left)
+                            this.padding.set(Insets.ZERO)
+                            this.markup.set(descMarkup)
+                            this.doLineWrapping.set(true)
+                        }
+                    }
+                }
+                entireVBox += RectElement(Color.DARK_GRAY).apply {
+                    this.bounds.height.set(72f)
+                    this.padding.set(Insets(6f))
+                    this += entire
+                }
+            }
+
+            entireVBox.sizeHeightToChildren(10f)
+            panePerCategory[category] = entireVBox
+        }
+        
+        val viewingCategory = Var<AchievementCategory?>(null)
+        
+        fun updateCategory() {
+            vbox.children.forEach(vbox::removeChild)
+            vbox.temporarilyDisableLayouts {
+                vbox += totalProgressLabel
+                (viewingCategory.getOrCompute()?.let { listOf(panePerCategory.getValue(it)) } ?: panePerCategory.values.toList()).forEach { 
+                    vbox += it
+                }
+            }
+            
+            vbox.sizeHeightToChildren(100f)
+            scrollPane.setContent(vbox)
+        }
+        
+        updateCategory()
+        viewingCategory.addListener {
+            updateCategory()
+        }
 
         hbox.temporarilyDisableLayouts {
             hbox += createSmallButton(binding = { Localization.getVar("common.back").use() }).apply {
                 this.bounds.width.set(100f)
                 this.setOnAction {
                     menuCol.popLastMenu()
+                }
+            }
+
+            hbox += ComboBox<AchievementCategory?>((listOf(null) + AchievementCategory.VALUES), viewingCategory.getOrCompute(), font = font).apply {
+                this.bounds.width.set(250f)
+                this.setScaleXY(0.85f)
+                this.itemStringConverter.bind {
+                    val viewAll = AchievementsL10N.getVar("achievement.viewAll").use()
+                    fun (ac: AchievementCategory?): String {
+                        return if (ac != null) {
+                            AchievementsL10N.getVar(ac.toLocalizationID()).use()
+                        } else viewAll
+                    }
+                }
+                this.onItemSelected = { newItem ->
+                    viewingCategory.set(newItem)
                 }
             }
         }
