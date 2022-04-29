8f78c99887dfb75748303b9108f0c1fc21dbbec2
chrislo27 <kinchrislo@gmail.com>
2022-04-29 15:11:30 -0700

    Track name will be greyed out if current drag or block is incompatible

--- core/src/main/kotlin/polyrhythmmania/editor/pane/track/EditorTrackSidePane.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/pane/track/EditorTrackSidePane.kt
@@ -1,12 +1,10 @@
 package polyrhythmmania.editor.pane.track
 
+import com.badlogic.gdx.graphics.Color
 import paintbox.ui.Pane
 import polyrhythmmania.Localization
-import polyrhythmmania.editor.Editor
-import polyrhythmmania.editor.TrackView
+import polyrhythmmania.editor.*
 import polyrhythmmania.editor.pane.EditorPane
-import polyrhythmmania.editor.Track
-import polyrhythmmania.editor.TrackID
 
 
 /**
@@ -28,5 +26,26 @@ class EditorTrackSidePane(val allTracksPane: AllTracksPane, val trackID: TrackID
         
         this.sidePanel.titleLabel.markup.set(editorPane.palette.markupBordered)
         this.sidePanel.titleText.bind { Localization.getVar("editor.track.${trackID.id}").use() }
+        this.sidePanel.titleLabel.textColor.bind { 
+            // Grey out when it is a disallowed track for the active instantiator OR drag
+            val allowed = Color.WHITE
+            val disallowed = Color.LIGHT_GRAY
+
+            val instantiatorList = editorPane.upperPane.instantiatorPane.instantiatorList
+            val allowedTracks = instantiatorList.currentItem.use().allowedTracks
+            val thisTrack = editor.trackMap[trackID]
+            val thisTrackTypes = thisTrack?.allowedTypes ?: emptySet()
+            
+            val click = editor.click.use()
+            if (click is Click.DragSelection) {
+                if (!click.tracksThatWillAccept.any { t -> t.allowedTypes.any { bt -> bt in thisTrackTypes } }) {
+                    disallowed
+                } else allowed
+            } else {
+                if (allowedTracks != null && !allowedTracks.any { bt -> bt in thisTrackTypes }) {
+                    disallowed
+                } else allowed
+            }
+        }
     }
 }
