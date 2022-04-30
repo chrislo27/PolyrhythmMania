da6777f9f3537e43236b44921ec50326c10dc250
chrislo27 <kinchrislo@gmail.com>
2022-04-30 14:39:33 -0700

    Add achievement hooks into NewEnginePlayScreenBase

--- core/src/main/kotlin/polyrhythmmania/screen/newplay/NewEnginePlayScreenBase.kt
+++ core/src/main/kotlin/polyrhythmmania/screen/newplay/NewEnginePlayScreenBase.kt
@@ -1,13 +1,18 @@
 package polyrhythmmania.screen.newplay
 
+import com.badlogic.gdx.Gdx
 import polyrhythmmania.PRManiaGame
+import polyrhythmmania.achievements.Achievements
 import polyrhythmmania.container.Container
 import polyrhythmmania.engine.InputCalibration
 import polyrhythmmania.engine.input.Challenges
 import polyrhythmmania.engine.input.Score
 import polyrhythmmania.screen.play.ResultsBehaviour
 import polyrhythmmania.sidemodes.SideMode
+import polyrhythmmania.sidemodes.endlessmode.EndlessPolyrhythm
 import polyrhythmmania.statistics.PlayTimeType
+import polyrhythmmania.world.EndlessType
+import polyrhythmmania.world.WorldType
 
 
 class NewEnginePlayScreenBase(
@@ -18,6 +23,57 @@ class NewEnginePlayScreenBase(
 ) : NewAbstractEnginePlayScreen(main, playTimeType, container, challenges, inputCalibration, sideMode, resultsBehaviour) {
 
     companion object; // Used for early init
+
+    private var endlessPrPauseTime: Float = 0f
+    
+    init {
+        // TODO move me
+        // Score achievements for endless-type modes
+        engine.inputter.endlessScore.score.addListener { scoreVar ->
+            if (engine.world.worldMode.endlessType == EndlessType.REGULAR_ENDLESS && engine.areStatisticsEnabled) {
+                val newScore = scoreVar.getOrCompute()
+                when (engine.world.worldMode.type) {
+                    WorldType.POLYRHYTHM -> {
+                        if (sideMode is EndlessPolyrhythm) {
+                            if (sideMode.dailyChallenge != null) {
+                                listOf(Achievements.dailyScore25, Achievements.dailyScore50,
+                                        Achievements.dailyScore75, Achievements.dailyScore100,
+                                        Achievements.dailyScore125).forEach {
+                                    Achievements.attemptAwardScoreAchievement(it, newScore)
+                                }
+                            } else {
+                                listOf(Achievements.endlessScore25, Achievements.endlessScore50,
+                                        Achievements.endlessScore75, Achievements.endlessScore100,
+                                        Achievements.endlessScore125).forEach {
+                                    Achievements.attemptAwardScoreAchievement(it, newScore)
+                                }
+
+                                if (sideMode.disableLifeRegen) {
+                                    Achievements.attemptAwardScoreAchievement(Achievements.endlessNoLifeRegen100, newScore)
+                                }
+                                if (engine.inputter.endlessScore.maxLives.get() == 1) { // Daredevil
+                                    Achievements.attemptAwardScoreAchievement(Achievements.endlessDaredevil100, newScore)
+                                }
+                                if (main.settings.masterVolumeSetting.getOrCompute() == 0) {
+                                    Achievements.attemptAwardScoreAchievement(Achievements.endlessSilent50, newScore)
+                                }
+                            }
+                        }
+
+                    }
+                    WorldType.DUNK -> {
+                        listOf(Achievements.dunkScore10, Achievements.dunkScore20, Achievements.dunkScore30,
+                                Achievements.dunkScore50).forEach {
+                            Achievements.attemptAwardScoreAchievement(it, newScore)
+                        }
+                    }
+                    WorldType.ASSEMBLE -> {
+                        // NO-OP
+                    }
+                }
+            }
+        }
+    }
     
     override fun copyThisScreenForResults(scoreObj: Score, resultsBehaviour: ResultsBehaviour): NewAbstractEnginePlayScreen {
         return NewEnginePlayScreenBase(main, playTimeType, container, challenges, inputCalibration, sideMode,
@@ -27,5 +83,26 @@ class NewEnginePlayScreenBase(
                     else resultsBehaviour.previousHighScore)
                 else resultsBehaviour)
     }
-    
+
+    override fun renderGameplay(delta: Float) {
+        super.renderGameplay(delta)
+
+        if (isPaused.get()) {
+            endlessPrPauseTime += Gdx.graphics.deltaTime
+        }
+    }
+
+    override fun pauseGame(playSound: Boolean) {
+        super.pauseGame(playSound)
+
+        endlessPrPauseTime = 0f
+    }
+
+    override fun unpauseGame(playSound: Boolean) {
+        super.unpauseGame(playSound)
+
+        if (sideMode != null && sideMode is EndlessPolyrhythm) {
+            sideMode.submitPauseTime(this.endlessPrPauseTime)
+        }
+    }
 }
