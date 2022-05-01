121973a32fa3ca287b5e314c4d0e2f68c9d6419f
chrislo27 <kinchrislo@gmail.com>
2022-05-01 13:58:30 -0700

    Change margin on language options

--- core/src/main/kotlin/polyrhythmmania/screen/mainmenu/menu/LanguageMenu.kt
+++ core/src/main/kotlin/polyrhythmmania/screen/mainmenu/menu/LanguageMenu.kt
@@ -78,6 +78,7 @@ class LanguageMenu(menuCol: MenuCollection) : StandardMenu(menuCol) {
                     settings.locale.set("${locale.language}_${locale.country}_${locale.variant}")
                 }
                 vbox += pane
+                button.textLabel.margin.set(Insets(0f, 0f, 10f, 0f))
                 
                 if (bundle == Localization.currentBundle.getOrCompute() || index == 0) {
                     button.selectedState.set(true)
