2f3c15d1258d7b10bb1b6664b9c8a058316257a8
chrislo27 <kinchrislo@gmail.com>
2022-05-08 16:48:04 -0700

    Add don't affect tail end to despawn pattern block

--- core/src/main/kotlin/polyrhythmmania/editor/block/BlockDespawnPattern.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/block/BlockDespawnPattern.kt
@@ -1,9 +1,12 @@
 package polyrhythmmania.editor.block
 
 import com.eclipsesource.json.JsonObject
+import paintbox.binding.BooleanVar
 import paintbox.binding.Var
 import paintbox.font.TextAlign
+import paintbox.ui.contextmenu.CheckBoxMenuItem
 import paintbox.ui.contextmenu.ContextMenu
+import paintbox.ui.contextmenu.SeparatorMenuItem
 import polyrhythmmania.Localization
 import polyrhythmmania.editor.Editor
 import polyrhythmmania.engine.Engine
@@ -19,6 +22,7 @@ class BlockDespawnPattern(engine: Engine) : Block(engine, BlockDespawnPattern.BL
     }
     
     val rowData: RowBlockData = RowBlockData(RowSetting.BOTH)
+    val disableTailEnd: BooleanVar = BooleanVar(false)
     
     init {
         this.width = 1f
@@ -34,14 +38,27 @@ class BlockDespawnPattern(engine: Engine) : Block(engine, BlockDespawnPattern.BL
     
     override fun compileIntoEvents(): List<Event> {
         val b = this.beat
-        return RowSetting.getRows(rowData.rowSetting.getOrCompute(), engine.world).map { row ->
-            EventRowBlockDespawn(engine, row, -1, b)
+        return if (disableTailEnd.get()) {
+            RowSetting.getRows(rowData.rowSetting.getOrCompute(), engine.world).flatMap { row ->
+                (0 until BlockSpawnPattern.ROW_COUNT).map { idx ->
+                    EventRowBlockDespawn(engine, row, idx, b)
+                }
+            }
+        } else {
+            RowSetting.getRows(rowData.rowSetting.getOrCompute(), engine.world).map { row ->
+                EventRowBlockDespawn(engine, row, -1, b)
+            }
         }
     }
 
     override fun createContextMenu(editor: Editor): ContextMenu {
         return ContextMenu().also { ctxmenu ->
+            ctxmenu.defaultWidth.set(350f)
             rowData.createMenuItems(editor).forEach { ctxmenu.addMenuItem(it) }
+            ctxmenu.addMenuItem(SeparatorMenuItem())
+            ctxmenu.addMenuItem(CheckBoxMenuItem.create(disableTailEnd,
+                    Localization.getValue("blockContextMenu.despawnPattern.disableTailEnd"),
+                    editor.editorPane.palette.markup))
         }
     }
 
@@ -49,16 +66,24 @@ class BlockDespawnPattern(engine: Engine) : Block(engine, BlockDespawnPattern.BL
         return BlockDespawnPattern(engine).also { 
             this.copyBaseInfoTo(it)
             it.rowData.rowSetting.set(this.rowData.rowSetting.getOrCompute())
+            it.disableTailEnd.set(this.disableTailEnd.get())
         }
     }
 
     override fun writeToJson(obj: JsonObject) {
         super.writeToJson(obj)
         rowData.writeToJson(obj)
+        if (disableTailEnd.get()) {
+            obj.add("disableTailEnd", true)
+        }
     }
 
     override fun readFromJson(obj: JsonObject) {
         super.readFromJson(obj)
         rowData.readFromJson(obj)
+        val disableTailEndValue = obj.get("disableTailEnd")
+        if (disableTailEndValue != null && disableTailEndValue.isBoolean) {
+            disableTailEnd.set(disableTailEndValue.asBoolean())
+        }
     }
 }
