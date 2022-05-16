db101a76072d7e4350cb61c75d64ea5a4b3003de
chrislo27 <kinchrislo@gmail.com>
2022-05-16 10:36:27 -0700

    Add data structures for contracts, conditions, prereqs

---
+++ core/src/main/kotlin/polyrhythmmania/storymode/contract/Condition.kt
@@ -0,0 +1,4 @@
+package polyrhythmmania.storymode.contract
+
+
+sealed class Condition
---
+++ core/src/main/kotlin/polyrhythmmania/storymode/contract/Contract.kt
@@ -0,0 +1,17 @@
+package polyrhythmmania.storymode.contract
+
+import paintbox.binding.ReadOnlyVar
+
+
+data class Contract(
+        val id: String,
+
+        val name: ReadOnlyVar<String>,
+        val desc: ReadOnlyVar<String>,
+        
+        val conditions: List<Condition>,
+
+        val fpPrereq: Int,
+        val fpReward: Int,
+        val prereqs: Set<Prereq> = emptySet(),
+)
