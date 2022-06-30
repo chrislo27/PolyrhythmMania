04004d5e43cd927dfd7e575aa8d0772c43d3d505
chrislo27 <kinchrislo@gmail.com>
2022-06-29 21:30:07 -0700

    Add blank barrage mode modifier module

---
+++ core/src/main/kotlin/polyrhythmmania/engine/modifiers/BarrageMode.kt
@@ -0,0 +1,19 @@
+package polyrhythmmania.engine.modifiers
+
+import polyrhythmmania.engine.input.EngineInputter
+import polyrhythmmania.engine.input.InputResult
+
+
+class BarrageMode : ModifierModule {
+    
+    // TODO implement barrage mode
+
+    override fun resetState() {
+    }
+    
+    override fun onMissed(inputter: EngineInputter, firstMiss: Boolean) {
+    }
+
+    override fun onInputResultHit(inputter: EngineInputter, result: InputResult, countsAsMiss: Boolean) {
+    }
+}
