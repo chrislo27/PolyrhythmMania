54a370346b00c3fb1603854a302f331d60e9c451
chrislo27 <kinchrislo@gmail.com>
2022-07-05 10:12:00 -0700

    Update flat block editor icons

--- core/src/main/kotlin/polyrhythmmania/editor/Palette.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/Palette.kt
@@ -120,5 +120,6 @@ open class Palette(val main: PRManiaGame) {
     val blockFlatPistonDpadOpenRegion: TextureRegion by lazy { TextureRegion(blockFlatTexture, 16 * 2, 16 * 2, 16, 16) }
     val blockFlatNoChangeRegion: TextureRegion by lazy { TextureRegion(blockFlatTexture, 16 * 3, 16 * 2, 16, 16) }
     val blockFlatRetractRegion: TextureRegion by lazy { TextureRegion(blockFlatTexture, 16 * 0, 16 * 3, 16, 16) }
+    val blockFlatSpotlightRegion: TextureRegion by lazy { TextureRegion(blockFlatTexture, 16 * 1, 16 * 3, 16, 16) }
     
 }
