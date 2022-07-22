f288ba91e9f10d03b41683ef8fe8f1645407e171
chrislo27 <kinchrislo@gmail.com>
2022-07-22 15:32:05 -0700

    Refactor timing computation for spawn pattern block

--- core/src/main/kotlin/polyrhythmmania/editor/block/BlockSpawnPattern.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/block/BlockSpawnPattern.kt
@@ -23,6 +23,67 @@ open class BlockSpawnPattern(engine: Engine) : Block(engine, BLOCK_TYPES) {
         val ROW_COUNT: Int = 10
         val ALLOWED_CUBE_TYPES: List<CubeType> by lazy { CubePatternData.GENERAL_CUBE_TYPES }
         val BLOCK_TYPES: EnumSet<BlockType> = EnumSet.of(BlockType.INPUT)
+
+        /**
+         * Returns a float array of size [rowArray] indicating the timing offset in beats for each block.
+         */
+        fun computeTimingOffsetsForRow(rowArray: List<CubeType>, beatsPerBlock: Float): FloatArray {
+            /*
+             - Let f(x) be the default time a block shows up based on its position x: f(x) = x * beatsPerBlock
+             - Pistons always show up at the time based on their position. Always uses f(x)
+             - Platforms:
+               - Figure out the contiguous section of platforms
+               - If the section ends with a piston, then the section of platforms appears when that piston does (use f(x) where x = piston)
+               - Otherwise, the section appears when the first platform in the section appears (use f(x) where x = first of the section)
+             - Tail end handling:
+               - If the very last index, l...
+                 - is a piston: fill in the rest of the blocks 2 time-units later (use f(l + 2)) (Piston X Platform Platform Platform...)
+                 - is a platform: fill in the rest of the blocks at the same time of l (NOT f(l), but whatever was computed for it earlier) (Platform Platform Platform...)
+                 - is nothing: fill in the rest of the blocks at the time of the first end index (use f(l + 1))
+             */
+            
+            val timings = FloatArray(rowArray.size) { idx -> idx * beatsPerBlock }
+
+            // Search for contiguous platform sections
+            var index = 0
+            val maxSearchIndex = rowArray.size
+            while (index in 0 until maxSearchIndex) {
+                val type = rowArray[index]
+                if (type == CubeType.PLATFORM && index < maxSearchIndex - 1) {
+                    // Find contiguous section of platforms
+                    val startIndexOfPlatforms = index
+                    var subindex = startIndexOfPlatforms + 1
+                    var endType: CubeType = rowArray[subindex]
+                    while (subindex in 0 until maxSearchIndex) { // While subindex is still in searchable region
+                        endType = rowArray[subindex]
+                        if (endType != CubeType.PLATFORM) break // Break at the first non-platform
+                        subindex++
+                    }
+                    // At the end, subindex = index where we ran out or it is not a platform, endType = rowArray[subindex]
+
+                    when (endType) {
+                        CubeType.PISTON -> {
+                            // Ends with a piston. All timings for the platform match the piston's
+                            for (i in index until subindex) {
+                                timings[i] = timings[subindex]
+                            }
+                        }
+                        else -> {
+                            // Set all timings for the platform to the firs tof the platform
+                            for (i in index until subindex) {
+                                timings[i] = timings[index]
+                            }
+                        }
+                    }
+                    
+                    index = subindex + 1 // Jump ahead past the ending. Don't need to check subindex since it won't be a platform
+                } else {
+                    index++
+                }
+            }
+            
+            return timings
+        } 
     }
 
     var patternData: CubePatternData = CubePatternData(ROW_COUNT, ALLOWED_CUBE_TYPES, CubeType.NONE).also { patternData ->
@@ -60,54 +121,11 @@ open class BlockSpawnPattern(engine: Engine) : Block(engine, BLOCK_TYPES) {
         return compileIntoEvents(0f, 0f)
     }
 
-    private fun compileRow(beat: Float, rowArray: MutableList<CubeType>, row: Row, pistonType: EntityPiston.Type, beatsPerBlock: Float): List<Event> {
+    private fun compileRow(beat: Float, rowArray: List<CubeType>, row: Row, pistonType: EntityPiston.Type, beatsPerBlock: Float): List<Event> {
         val events = mutableListOf<Event>()
+        val timings: FloatArray = computeTimingOffsetsForRow(rowArray, beatsPerBlock)
 
-        /*
-        - Pistons show up at the time based on their position
-        - Platforms show up when:
-          - If the section encounters a piston, when said piston appears
-          - Otherwise, when the FIRST platform should appear
-        - If the very last index
-          - is a piston: fill in the rest of the blocks 2 after
-          - is a platform: fill in the rest of the blocks at the same time
-          - is nothing: fill in the rest of the blocks at the time of the first end index
-        */
-
-        val timings: FloatArray = FloatArray(rowArray.size) { it * beatsPerBlock }
-
-        var index: Int = 0
-        while (index in 0 until rowArray.size) {
-            val type = rowArray[index]
-            if (type == CubeType.PLATFORM && index < rowArray.size - 1) {
-                // Find contiguous section
-                var subindex = index + 1
-                var endType: CubeType = rowArray[subindex]
-                while (subindex in 0 until rowArray.size) {
-                    endType = rowArray[subindex]
-                    if (endType != CubeType.PLATFORM) break
-                    subindex++
-                }
-
-                when (endType) {
-                    CubeType.PISTON -> {
-                        for (i in index until subindex) {
-                            timings[i] = timings[subindex]
-                        }
-                        index = subindex
-                    }
-                    else -> {
-                        for (i in index until subindex) {
-                            timings[i] = timings[index]
-                        }
-                        index = subindex
-                    }
-                }
-            }
-
-            index++
-        }
-
+        // Populate events and handle tail end platforms
         var anyNotNone = false
         timings.forEachIndexed { ind, b ->
             val cube = rowArray[ind]
@@ -120,12 +138,17 @@ open class BlockSpawnPattern(engine: Engine) : Block(engine, BLOCK_TYPES) {
 
             if (ind == timings.size - 1 && anyNotNone && !disableTailEnd.get()) {
                 when (cube) {
-                    CubeType.NO_CHANGE -> {}
-                    CubeType.NONE -> {
-                        val next = ind + 1
+                    CubeType.NO_CHANGE, CubeType.RETRACT_PISTON -> {
+                        // NO-OP
+                    }
+                    CubeType.PISTON, CubeType.PISTON_OPEN -> {
+                        val next = ind + 2
                         if (next < row.length) {
-                            events += EventRowBlockSpawn(engine, row, next, EntityPiston.Type.PLATFORM,
-                                    beat + next * beatsPerBlock, affectThisIndexAndForward = true)
+                            events += EventRowBlockSpawn(
+                                    engine, row, next, EntityPiston.Type.PLATFORM,
+                                    beat + next * beatsPerBlock, affectThisIndexAndForward = true,
+                                    startPistonExtended = cube == CubeType.PISTON_OPEN
+                            )
                         }
                     }
                     CubeType.PLATFORM -> {
@@ -135,17 +158,13 @@ open class BlockSpawnPattern(engine: Engine) : Block(engine, BLOCK_TYPES) {
                                     beat + b, affectThisIndexAndForward = true)
                         }
                     }
-                    CubeType.PISTON, CubeType.PISTON_OPEN -> {
-                        val next = ind + 2
+                    CubeType.NONE -> {
+                        val next = ind + 1
                         if (next < row.length) {
-                            events += EventRowBlockSpawn(
-                                engine, row, next, EntityPiston.Type.PLATFORM,
-                                beat + next * beatsPerBlock, affectThisIndexAndForward = true,
-                                startPistonExtended = cube == CubeType.PISTON_OPEN
-                            )
+                            events += EventRowBlockSpawn(engine, row, next, EntityPiston.Type.PLATFORM,
+                                    beat + next * beatsPerBlock, affectThisIndexAndForward = true)
                         }
                     }
-                    CubeType.RETRACT_PISTON -> { /* NO-OP, not needed for regular Spawn Pattern */ }
                 }
             }
         }
