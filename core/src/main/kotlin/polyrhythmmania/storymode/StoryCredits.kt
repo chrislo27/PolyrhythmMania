6c3804f157c6dafb3393e2d85a49033c5e8dc76b
chrislo27 <kinchrislo@gmail.com>
2022-11-25 18:10:43 -0800

    Update story credits for level creators

--- core/src/main/kotlin/polyrhythmmania/storymode/StoryCredits.kt
+++ core/src/main/kotlin/polyrhythmmania/storymode/StoryCredits.kt
@@ -1,18 +1,36 @@
 package polyrhythmmania.storymode
 
 import paintbox.binding.ReadOnlyVar
-import paintbox.binding.Var
 import polyrhythmmania.Localization
 import polyrhythmmania.credits.CreditsBase
-import java.util.*
+import polyrhythmmania.storymode.contract.Contracts
 
 
 object StoryCredits : CreditsBase() {
-    
+
     override val credits: Map<ReadOnlyVar<String>, List<ReadOnlyVar<String>>> = linkedMapOf(
-            Localization.getVar("credits.story.levelCreators") to abcSorted(
-                    "Dream Top", "Kievit", "J-D Thunder"
+            Localization.getVar("credits.story.levelCreators") to getLevelCreators().toVars(),
+            Localization.getVar("credits.story.writing") to abcSorted(
+                    "sp00pster", "chrislo27", "J-D Thunder"
             ).toVars(),
     )
-    
+
+    /**
+     * Returns all level creators sorted by level frequency and then alphabetical
+     */
+    private fun getLevelCreators(): List<String> {
+        return Contracts.contracts.values
+                .flatMap { it.attribution?.creators ?: emptyList() }
+                .groupBy { it }
+                .map { it.key to it.value.size }
+                .filter { it.second > 0 && it.first.isNotBlank() }
+                .sortedWith(
+                        Comparator<Pair<String, Int>> { o1, o2 ->
+                            o1.second.compareTo(o2.second)
+                        }.then { o1, o2 ->
+                            o1.first.compareTo(o2.first)
+                        }
+                )
+                .map { it.first }
+    }
 }
