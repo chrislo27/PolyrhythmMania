3a0aae50c2d45c25de3d6df084e7c915f6aecee5
chrislo27 <kinchrislo@gmail.com>
2022-12-11 16:31:30 -0800

    Desktop menu is collapsed by default, add open audio settings button

--- core/src/main/kotlin/polyrhythmmania/storymode/screen/desktop/DesktopDialogMenu.kt
+++ core/src/main/kotlin/polyrhythmmania/storymode/screen/desktop/DesktopDialogMenu.kt
@@ -1,13 +1,16 @@
 package polyrhythmmania.storymode.screen.desktop
 
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.Interpolation
 import paintbox.binding.BooleanVar
+import paintbox.binding.FloatVar
 import paintbox.binding.Var
 import paintbox.transition.FadeToOpaque
 import paintbox.transition.FadeToTransparent
 import paintbox.transition.TransitionScreen
 import paintbox.ui.RenderAlign
 import paintbox.ui.UIElement
+import paintbox.ui.animation.Animation
 import paintbox.ui.area.Insets
 import paintbox.ui.control.Button
 import paintbox.ui.control.Slider
@@ -24,7 +27,14 @@ import polyrhythmmania.ui.PRManiaSkins
 
 class DesktopDialogMenu(desktopUI: DesktopUI) : DesktopDialog(desktopUI) {
     
+    companion object {
+        private const val MAIN_PANE_HEIGHT_COLLAPSED: Float = 78f
+        private const val MAIN_PANE_HEIGHT_FULL: Float = 150f
+    }
+    
     private val resetVolumesToggle: BooleanVar = BooleanVar(false)
+    
+    private val mainPaneHeightUnits: FloatVar = FloatVar(MAIN_PANE_HEIGHT_COLLAPSED)
     
     init {
         val settings = main.settings
@@ -32,13 +42,14 @@ class DesktopDialogMenu(desktopUI: DesktopUI) : DesktopDialog(desktopUI) {
         val robotoBoldMarkup = desktopUI.robotoBoldMarkup
         
         this.mainPane.apply {
-            this.bounds.height.set(150f * UI_SCALE)
+            this.bounds.height.bind { mainPaneHeightUnits.use() * UI_SCALE }
         }
         this.mainPane += VBox().apply {
             fun separator(): UIElement = RectElement(Color().grey(0.8f)).apply {
                 this.bounds.height.set(0.5f * UI_SCALE)
             }
             
+            this.doClipping.set(true)
             this.spacing.set(3f * UI_SCALE)
 
             this += TextLabel(StoryL10N.getVar("desktop.menu"), font = main.fontMainMenuHeading).apply {
@@ -80,57 +91,76 @@ class DesktopDialogMenu(desktopUI: DesktopUI) : DesktopDialog(desktopUI) {
             this += separator()
             
             // Audio settings
-            this += TextLabel(StoryL10N.getVar("desktop.menu.settings.audio"), font = main.fontMainMenuHeading).apply {
-                this.bounds.height.set(7f * UI_SCALE)
-                this.textColor.set(Color.WHITE)
-                this.renderAlign.set(RenderAlign.center)
-                this.margin.set(Insets(1f, 1f, 4f, 4f))
-                this.setScaleXY(0.625f)
-            }
-            fun createVolumeSlider(nameKey: String, setting: Var<Int>, resetValue: Int) {
-                this += VBox().apply {
-                    this.bounds.height.set(13f * UI_SCALE)
-                    this.spacing.set(0.5f * UI_SCALE)
+            fun addAudioSettings() {
+                this += TextLabel(StoryL10N.getVar("desktop.menu.settings.audio"), font = main.fontMainMenuHeading).apply {
+                    this.bounds.height.set(7f * UI_SCALE)
+                    this.textColor.set(Color.WHITE)
+                    this.renderAlign.set(RenderAlign.center)
+                    this.margin.set(Insets(1f, 1f, 4f, 4f))
+                    this.setScaleXY(0.625f)
+                }
+                fun createVolumeSlider(nameKey: String, setting: Var<Int>, resetValue: Int) {
+                    this += VBox().apply {
+                        this.bounds.height.set(13f * UI_SCALE)
+                        this.spacing.set(0.5f * UI_SCALE)
 
-                    this.temporarilyDisableLayouts {
-                        val slider = Slider().apply {
-                            this.bounds.height.set(6f * UI_SCALE)
-                            this.minimum.set(0f)
-                            this.maximum.set(100f)
-                            this.tickUnit.set(5f)
-                            this.setValue(setting.getOrCompute().toFloat())
-                            this.value.addListener { v ->
-                                setting.set(v.getOrCompute().toInt())
+                        this.temporarilyDisableLayouts {
+                            val slider = Slider().apply {
+                                this.bounds.height.set(6f * UI_SCALE)
+                                this.minimum.set(0f)
+                                this.maximum.set(100f)
+                                this.tickUnit.set(5f)
+                                this.setValue(setting.getOrCompute().toFloat())
+                                this.value.addListener { v ->
+                                    setting.set(v.getOrCompute().toInt())
+                                }
+                                resetVolumesToggle.addListener {
+                                    this.setValue(resetValue.toFloat())
+                                }
                             }
-                            resetVolumesToggle.addListener {
-                                this.setValue(resetValue.toFloat())
+                            this += TextLabel(StoryL10N.getVar("desktop.menu.settings.audio.settingLabel", Var.bind {
+                                listOf(Localization.getVar(nameKey).use(), slider.value.use().toInt())
+                            })).apply {
+                                this.markup.set(robotoRegularMarkup)
+                                this.bounds.height.set(6f * UI_SCALE)
+                                this.textColor.set(Color.WHITE)
+                                this.renderAlign.set(RenderAlign.left)
+                                this.margin.set(Insets(1f, 1f, 4f, 4f))
                             }
+                            this += slider
                         }
-                        this += TextLabel(StoryL10N.getVar("desktop.menu.settings.audio.settingLabel", Var.bind {
-                            listOf(Localization.getVar(nameKey).use(), slider.value.use().toInt())
-                        })).apply {
-                            this.markup.set(robotoRegularMarkup)
-                            this.bounds.height.set(6f * UI_SCALE)
-                            this.textColor.set(Color.WHITE)
-                            this.renderAlign.set(RenderAlign.left)
-                            this.margin.set(Insets(1f, 1f, 4f, 4f))
-                        }
-                        this += slider
+                    }
+                }
+                createVolumeSlider("mainMenu.audioSettings.masterVol", settings.masterVolumeSetting, Settings.DEFAULT_MASTER_VOLUME)
+                createVolumeSlider("mainMenu.audioSettings.gameplayVol", settings.gameplayVolumeSetting, Settings.DEFAULT_SPECIFIC_VOLUME)
+                createVolumeSlider("mainMenu.audioSettings.menuMusicVol", settings.menuMusicVolumeSetting, Settings.DEFAULT_SPECIFIC_VOLUME)
+                createVolumeSlider("mainMenu.audioSettings.menuSfxVol", settings.menuSfxVolumeSetting, Settings.DEFAULT_SPECIFIC_VOLUME)
+                this += Button(Localization.getVar("mainMenu.audioSettings.resetLevels")).apply {
+                    this.skinID.set(PRManiaSkins.BUTTON_SKIN_STORY_LIGHT)
+                    this.markup.set(robotoRegularMarkup)
+                    this.bounds.height.set(8f * UI_SCALE)
+                    this.setScaleXY(0.9f)
+                    this.setOnAction {
+                        resetVolumesToggle.invert()
+                        desktopUI.controller.playSFX(DesktopController.SFXType.CLICK_INBOX_ITEM)
                     }
                 }
             }
-            createVolumeSlider("mainMenu.audioSettings.masterVol", settings.masterVolumeSetting, Settings.DEFAULT_MASTER_VOLUME)
-            createVolumeSlider("mainMenu.audioSettings.gameplayVol", settings.gameplayVolumeSetting, Settings.DEFAULT_SPECIFIC_VOLUME)
-            createVolumeSlider("mainMenu.audioSettings.menuMusicVol", settings.menuMusicVolumeSetting, Settings.DEFAULT_SPECIFIC_VOLUME)
-            createVolumeSlider("mainMenu.audioSettings.menuSfxVol", settings.menuSfxVolumeSetting, Settings.DEFAULT_SPECIFIC_VOLUME)
-            this += Button(Localization.getVar("mainMenu.audioSettings.resetLevels")).apply {
+            
+            val vbox = this
+            this += Button(StoryL10N.getVar("desktop.menu.settings.audio")).apply {
                 this.skinID.set(PRManiaSkins.BUTTON_SKIN_STORY_LIGHT)
                 this.markup.set(robotoRegularMarkup)
-                this.bounds.height.set(8f * UI_SCALE)
-                this.setScaleXY(0.9f)
+                this.bounds.height.set(10f * UI_SCALE)
                 this.setOnAction {
-                    resetVolumesToggle.invert()
-                    desktopUI.controller.playSFX(DesktopController.SFXType.CLICK_INBOX_ITEM)
+                    desktopUI.controller.playSFX(DesktopController.SFXType.PAUSE_ENTER)
+                    vbox.removeChild(this)
+                    vbox.temporarilyDisableLayouts {
+                        addAudioSettings()
+                    }
+                    desktopUI.sceneRoot.animations.enqueueAnimation(
+                            Animation(Interpolation.pow5Out, 0.25f, MAIN_PANE_HEIGHT_COLLAPSED, MAIN_PANE_HEIGHT_FULL),
+                            mainPaneHeightUnits)
                 }
             }
         }
