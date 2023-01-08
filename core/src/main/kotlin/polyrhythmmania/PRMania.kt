b78ae8a13a20ae631f09ed6d196bb9dc27bf37c2
chrislo27 <kinchrislo@gmail.com>
2023-01-08 10:00:45 -0800

    Bump alpha version for milestone 2

--- core/src/main/kotlin/polyrhythmmania/PRMania.kt
+++ core/src/main/kotlin/polyrhythmmania/PRMania.kt
@@ -15,7 +15,7 @@ object PRMania {
     const val GITHUB = "https://github.com/chrislo27/PolyrhythmMania"
     const val HOMEPAGE = "https://polyrhythmmania.rhre.dev"
     const val DONATE_LINK = "https://www.paypal.com/donate/?hosted_button_id=9JLGHKZNWLLQ8"
-    val VERSION: Version = Version(2, 0, 0, "dev_20221226a")
+    val VERSION: Version = Version(2, 0, 0, "alpha_M2_20230108a")
     const val WIDTH: Int = 1280
     const val HEIGHT: Int = 720
     val DEFAULT_SIZE: WindowSize = WindowSize(WIDTH, HEIGHT)
