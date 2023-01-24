6c5d29afe01eab371430f3dddf5677aaa76a9236
chrislo27 <kinchrislo@gmail.com>
2023-01-24 06:44:59 -0800

    Reformat desktop project with official style guide

--- desktop/src/main/kotlin/polyrhythmmania/desktop/DesktopLauncher.kt
+++ desktop/src/main/kotlin/polyrhythmmania/desktop/DesktopLauncher.kt
@@ -50,20 +50,26 @@ object DesktopLauncher {
                 PRMania.possiblyNewPortableMode = true
             }
         }
-        
-        val app = PRManiaGame(PRManiaGame.createPaintboxSettings(args.toList(), Logger(), PRMania.MAIN_FOLDER.resolve("logs/")))
-        
+
+        val app = PRManiaGame(
+            PRManiaGame.createPaintboxSettings(
+                args.toList(),
+                Logger(),
+                PRMania.MAIN_FOLDER.resolve("logs/")
+            )
+        )
+
         PRMania.logMissingLocalizations = arguments.logMissingLocalizations
         PRMania.dumpPackedSheets = arguments.dumpPackedSheets
         PRMania.enableMetrics = arguments.enableMetrics
         if (arguments.audioDeviceBufferSize != null || arguments.audioDeviceBufferCount != null) {
             val audioDeviceSettings = AudioDeviceSettings(
-                    arguments.audioDeviceBufferSize ?: AudioDeviceSettings.getDefaultBufferSize(),
-                    arguments.audioDeviceBufferCount ?: AudioDeviceSettings.getDefaultBufferCount()
+                arguments.audioDeviceBufferSize ?: AudioDeviceSettings.getDefaultBufferSize(),
+                arguments.audioDeviceBufferCount ?: AudioDeviceSettings.getDefaultBufferCount()
             )
             PRMania.audioDeviceSettings = audioDeviceSettings
         }
-        
+
         PaintboxDesktopLauncher(app, arguments).editConfig {
             this.setAutoIconify(true)
             val emulatedSize = app.paintboxSettings.emulatedSize
@@ -74,12 +80,17 @@ object DesktopLauncher {
             this.setInitialVisible(false)
             this.setInitialBackgroundColor(Color(0f, 0f, 0f, 1f))
             // Note: the buffer size and count here are largely ignored since we don't use Gdx.audio.newAudioDevice
-            this.setAudioConfig(100, PRMania.audioDeviceSettings?.bufferSize ?: AudioDeviceSettings.getDefaultBufferSize(),
-                    PRMania.audioDeviceSettings?.bufferCount ?: AudioDeviceSettings.getDefaultBufferCount())
+            this.setAudioConfig(
+                100, PRMania.audioDeviceSettings?.bufferSize ?: AudioDeviceSettings.getDefaultBufferSize(),
+                PRMania.audioDeviceSettings?.bufferCount ?: AudioDeviceSettings.getDefaultBufferCount()
+            )
             this.setHdpiMode(HdpiMode.Logical)
             this.setBackBufferConfig(8, 8, 8, 8, 16, 0, /* samples = */ 2)
-            this.setPreferencesConfig(".polyrhythmmania/prefs/", if (portableMode) Files.FileType.Local else Files.FileType.External)
-            
+            this.setPreferencesConfig(
+                ".polyrhythmmania/prefs/",
+                if (portableMode) Files.FileType.Local else Files.FileType.External
+            )
+
             val sizes: List<Int> = listOf(32, 24, 16)
             this.setWindowIcon(Files.FileType.Internal, *sizes.map { "icon/$it.png" }.toTypedArray())
         }.launch()
--- desktop/src/main/kotlin/polyrhythmmania/desktop/PRManiaArguments.kt
+++ desktop/src/main/kotlin/polyrhythmmania/desktop/PRManiaArguments.kt
@@ -6,23 +6,38 @@ import polyrhythmmania.soundsystem.AudioDeviceSettings
 
 class PRManiaArguments : PaintboxArguments() {
 
-    @Parameter(names = ["--log-missing-localizations"], description = "Logs any missing localizations. Other locales are checked against the default properties file.")
+    @Parameter(
+        names = ["--log-missing-localizations"],
+        description = "Logs any missing localizations. Other locales are checked against the default properties file."
+    )
     var logMissingLocalizations: Boolean = false
 
     @Parameter(names = ["--dump-packed-sheets"], description = "Dump a copy of every packed sheet to a temp folder.")
     var dumpPackedSheets: Boolean = false
-    
-    @Parameter(names = ["--portable-mode"], description = "The .polyrhythmmania/ directory will be local to the game executable instead of the user home.")
+
+    @Parameter(
+        names = ["--portable-mode"],
+        description = "The .polyrhythmmania/ directory will be local to the game executable instead of the user home."
+    )
     var portableMode: Boolean = false
-    
-    @Parameter(names = ["--enable-metrics"], description = "Enables recording of certain program metrics. Used for debugging.")
+
+    @Parameter(
+        names = ["--enable-metrics"],
+        description = "Enables recording of certain program metrics. Used for debugging."
+    )
     var enableMetrics: Boolean = false
-    
-    @Parameter(names = ["--audio-device-buffer-size"], description = "Overrides the AudioDevice buffer size when using the OpenAL sound system. Should be a power of two and at least ${AudioDeviceSettings.MINIMUM_SIZE}. On Windows, defaults to ${AudioDeviceSettings.DEFAULT_SIZE_WINDOWS}; ${AudioDeviceSettings.DEFAULT_SIZE} on other platforms.")
+
+    @Parameter(
+        names = ["--audio-device-buffer-size"],
+        description = "Overrides the AudioDevice buffer size when using the OpenAL sound system. Should be a power of two and at least ${AudioDeviceSettings.MINIMUM_SIZE}. On Windows, defaults to ${AudioDeviceSettings.DEFAULT_SIZE_WINDOWS}; ${AudioDeviceSettings.DEFAULT_SIZE} on other platforms."
+    )
     var audioDeviceBufferSize: Int? = null
-    
+
     // If this param name is changed, update AdvAudioMenu
-    @Parameter(names = ["--audio-device-buffer-count"], description = "Overrides the AudioDevice buffer count when using the OpenAL sound system. Should be at least ${AudioDeviceSettings.MINIMUM_COUNT}. On Windows, defaults to ${AudioDeviceSettings.DEFAULT_COUNT_WINDOWS}; ${AudioDeviceSettings.DEFAULT_COUNT} on other platforms.")
+    @Parameter(
+        names = ["--audio-device-buffer-count"],
+        description = "Overrides the AudioDevice buffer count when using the OpenAL sound system. Should be at least ${AudioDeviceSettings.MINIMUM_COUNT}. On Windows, defaults to ${AudioDeviceSettings.DEFAULT_COUNT_WINDOWS}; ${AudioDeviceSettings.DEFAULT_COUNT} on other platforms."
+    )
     var audioDeviceBufferCount: Int? = null
-    
+
 }
