d01c249babdbeeb509b2ae8884a65ee21cd4f5dd
chrislo27 <kinchrislo@gmail.com>
2023-09-09 14:02:40 -0700

    Add tooltip and reset button for CustomSpeedBlock context menu

--- core/src/main/kotlin/polyrhythmmania/editor/block/CustomSpeedBlock.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/block/CustomSpeedBlock.kt
@@ -1,17 +1,20 @@
 package polyrhythmmania.editor.block
 
 import com.badlogic.gdx.graphics.Color
+import paintbox.ui.Anchor
 import paintbox.ui.area.Insets
 import paintbox.ui.border.SolidBorder
 import paintbox.ui.contextmenu.ContextMenu
 import paintbox.ui.contextmenu.CustomMenuItem
 import paintbox.ui.contextmenu.LabelMenuItem
+import paintbox.ui.control.Button
 import paintbox.ui.control.DecimalTextField
 import paintbox.ui.element.RectElement
 import paintbox.ui.layout.HBox
 import paintbox.util.DecimalFormats
 import polyrhythmmania.Localization
 import polyrhythmmania.editor.Editor
+import polyrhythmmania.editor.block.contextmenu.AbstractPatternMenuPane
 import polyrhythmmania.world.entity.EntityRodDecor
 import java.text.DecimalFormat
 
@@ -24,6 +27,8 @@ interface CustomSpeedBlock {
         const val MIN_X_UNITS_PER_BEAT: Float = 0.25f
         
         val INPUT_DECIMAL_FORMAT: DecimalFormat = DecimalFormats["0.0##"]
+        
+        private fun invertButtonSkin(button: Button) = AbstractPatternMenuPane.invertButtonSkin(button)
     }
 
     var xUnitsPerBeat: Float
@@ -38,18 +43,26 @@ interface CustomSpeedBlock {
                         1f / DEFAULT_X_UNITS_PER_BEAT
                     ),
                     editor.editorPane.palette.markup
-                )
+                ).apply {
+                    this.createTooltip = { v ->
+                        v.set(editor.editorPane.createDefaultTooltip(Localization.getValue("blockContextMenu.customSpeedBlock.beatsPerBlock.tooltip")))
+                    }
+                }
             )
             ctxmenu.addMenuItem(CustomMenuItem(
                 HBox().apply {
-                    this.bounds.width.set(175f)
                     this.bounds.height.set(32f)
                     this.spacing.set(4f)
+                    
+                    val textField: DecimalTextField
+                    
                     this += RectElement(Color(0f, 0f, 0f, 1f)).apply {
+                        this.bounds.width.set(175f)
+                        
                         this.border.set(Insets(1f))
                         this.borderStyle.set(SolidBorder(Color.WHITE))
                         this.padding.set(Insets(2f))
-                        this += DecimalTextField(
+                        textField = DecimalTextField(
                             startingValue = 1f / xUnitsPerBeat,
                             decimalFormat = INPUT_DECIMAL_FORMAT,
                             font = editor.editorPane.palette.musicDialogFont
@@ -60,6 +73,19 @@ interface CustomSpeedBlock {
                             this.value.addListener {
                                 xUnitsPerBeat = 1f / it.getOrCompute().coerceAtLeast(0.25f)
                             }
+                        }
+                        this += textField
+                    }
+
+                    this += Button(
+                        Localization.getVar("common.reset"),
+                        font = editor.editorPane.main.fontEditor
+                    ).apply {
+                        Anchor.CentreRight.configure(this)
+                        this.bounds.width.set(64f)
+                        invertButtonSkin(this)
+                        this.setOnAction {
+                            textField.setValue(1f / DEFAULT_X_UNITS_PER_BEAT)
                         }
                     }
                 }
