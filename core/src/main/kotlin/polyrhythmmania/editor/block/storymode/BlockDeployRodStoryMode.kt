4a903bfa95964507689f43b4c707580f8a90c9c3
chrislo27 <kinchrislo@gmail.com>
2023-09-10 08:13:08 -0700

    Move story mode custom speed blocks to use same interface

--- core/src/main/kotlin/polyrhythmmania/editor/block/storymode/BlockDeployRodStoryMode.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/block/storymode/BlockDeployRodStoryMode.kt
@@ -1,19 +1,15 @@
 package polyrhythmmania.editor.block.storymode
 
-import com.badlogic.gdx.graphics.Color
 import com.eclipsesource.json.JsonObject
 import paintbox.binding.BooleanVar
-import paintbox.ui.area.Insets
-import paintbox.ui.border.SolidBorder
-import paintbox.ui.contextmenu.*
-import paintbox.ui.control.DecimalTextField
-import paintbox.ui.element.RectElement
-import paintbox.ui.layout.HBox
-import paintbox.util.DecimalFormats
+import paintbox.ui.contextmenu.CheckBoxMenuItem
+import paintbox.ui.contextmenu.ContextMenu
+import paintbox.ui.contextmenu.SeparatorMenuItem
 import polyrhythmmania.editor.Editor
 import polyrhythmmania.editor.EditorSpecialFlags
 import polyrhythmmania.editor.block.BlockDeployRod
 import polyrhythmmania.editor.block.BlockType
+import polyrhythmmania.editor.block.CustomSpeedBlock
 import polyrhythmmania.editor.block.RowSetting
 import polyrhythmmania.engine.Engine
 import polyrhythmmania.engine.Event
@@ -23,9 +19,9 @@ import java.util.*
 
 
 class BlockDeployRodStoryMode(engine: Engine, blockTypes: EnumSet<BlockType> = BLOCK_TYPES)
-    : BlockDeployRod(engine, blockTypes) {
+    : BlockDeployRod(engine, blockTypes), CustomSpeedBlock {
 
-    var xUnitsPerBeat: Float = EntityRodDecor.DEFAULT_X_UNITS_PER_BEAT
+    override var xUnitsPerBeat: Float = EntityRodDecor.DEFAULT_X_UNITS_PER_BEAT
     val defective: BooleanVar = BooleanVar(false)
     
     init {
@@ -56,30 +52,11 @@ class BlockDeployRodStoryMode(engine: Engine, blockTypes: EnumSet<BlockType> = B
 
     override fun createContextMenu(editor: Editor): ContextMenu {
         return super.createContextMenu(editor).also { ctxmenu ->
+            ctxmenu.defaultWidth.set(352f)
             ctxmenu.addMenuItem(SeparatorMenuItem())
             ctxmenu.addMenuItem(CheckBoxMenuItem.create(defective, "Defective?", editor.editorPane.palette.markup))
             ctxmenu.addMenuItem(SeparatorMenuItem())
-            ctxmenu.addMenuItem(LabelMenuItem.create("Beats per block (default ${1f / EntityRodDecor.DEFAULT_X_UNITS_PER_BEAT})", editor.editorPane.palette.markup))
-            ctxmenu.addMenuItem(CustomMenuItem(
-                HBox().apply {
-                    this.bounds.height.set(32f)
-                    this.spacing.set(4f)
-                    this += RectElement(Color(0f, 0f, 0f, 1f)).apply {
-                        this.border.set(Insets(1f))
-                        this.borderStyle.set(SolidBorder(Color.WHITE))
-                        this.padding.set(Insets(2f))
-                        this += DecimalTextField(startingValue = 1f / xUnitsPerBeat, decimalFormat = DecimalFormats["0.0##"],
-                            font = editor.editorPane.palette.musicDialogFont).apply {
-                            this.minimumValue.set(0.25f)
-                            this.textColor.set(Color(1f, 1f, 1f, 1f))
-
-                            this.value.addListener {
-                                xUnitsPerBeat = 1f / it.getOrCompute().coerceAtLeast(0.25f)
-                            }
-                        }
-                    }
-                }
-            ))
+            ctxmenu.addToContextMenu(editor)
         }
     }
 
--- core/src/main/kotlin/polyrhythmmania/editor/block/storymode/BlockSpawnPatternStoryMode.kt
+++ core/src/main/kotlin/polyrhythmmania/editor/block/storymode/BlockSpawnPatternStoryMode.kt
@@ -1,33 +1,25 @@
 package polyrhythmmania.editor.block.storymode
 
-import com.badlogic.gdx.graphics.Color
 import com.eclipsesource.json.JsonObject
-import paintbox.ui.area.Insets
-import paintbox.ui.border.SolidBorder
 import paintbox.ui.contextmenu.ContextMenu
-import paintbox.ui.contextmenu.CustomMenuItem
-import paintbox.ui.contextmenu.LabelMenuItem
 import paintbox.ui.contextmenu.SeparatorMenuItem
-import paintbox.ui.control.DecimalTextField
-import paintbox.ui.element.RectElement
-import paintbox.ui.layout.HBox
-import paintbox.util.DecimalFormats
 import polyrhythmmania.editor.Editor
 import polyrhythmmania.editor.EditorSpecialFlags
 import polyrhythmmania.editor.block.BlockSpawnPattern
+import polyrhythmmania.editor.block.CustomSpeedBlock
 import polyrhythmmania.engine.Engine
 import polyrhythmmania.world.entity.EntityRodDecor
 import java.util.*
 
 
-class BlockSpawnPatternStoryMode(engine: Engine) : BlockSpawnPattern(engine) {
+class BlockSpawnPatternStoryMode(engine: Engine) : BlockSpawnPattern(engine), CustomSpeedBlock {
 
     companion object {
 
         const val DEFAULT_X_UNITS_PER_BEAT: Float = EntityRodDecor.DEFAULT_X_UNITS_PER_BEAT
     }
 
-    private var xUnitsPerBeat: Float = DEFAULT_X_UNITS_PER_BEAT
+    override var xUnitsPerBeat: Float = DEFAULT_X_UNITS_PER_BEAT
     
     constructor(engine: Engine, xUnitsPerBeat: Float) : this(engine) {
         this.xUnitsPerBeat = xUnitsPerBeat
@@ -45,27 +37,7 @@ class BlockSpawnPatternStoryMode(engine: Engine) : BlockSpawnPattern(engine) {
     override fun createContextMenu(editor: Editor): ContextMenu {
         return super.createContextMenu(editor).also { ctxmenu ->
             ctxmenu.addMenuItem(SeparatorMenuItem())
-            ctxmenu.addMenuItem(LabelMenuItem.create("Beats per block (default ${1f / EntityRodDecor.DEFAULT_X_UNITS_PER_BEAT})", editor.editorPane.palette.markup))
-            ctxmenu.addMenuItem(CustomMenuItem(
-                HBox().apply {
-                    this.bounds.height.set(32f)
-                    this.spacing.set(4f)
-                    this += RectElement(Color(0f, 0f, 0f, 1f)).apply {
-                        this.border.set(Insets(1f))
-                        this.borderStyle.set(SolidBorder(Color.WHITE))
-                        this.padding.set(Insets(2f))
-                        this += DecimalTextField(startingValue = 1f / xUnitsPerBeat, decimalFormat = DecimalFormats["0.0##"],
-                            font = editor.editorPane.palette.musicDialogFont).apply {
-                            this.minimumValue.set(0.25f)
-                            this.textColor.set(Color(1f, 1f, 1f, 1f))
-
-                            this.value.addListener {
-                                xUnitsPerBeat = 1f / it.getOrCompute().coerceAtLeast(0.25f)
-                            }
-                        }
-                    }
-                }
-            ))
+            ctxmenu.addToContextMenu(editor)
         }
     }
 
